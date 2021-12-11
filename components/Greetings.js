3438d70e73a92dfcd0c1422a3c65523acee7e5ab
lunghile-suc <lunghilesuccess1@gmail.com>
2021-12-11 08:57:13 +0200

    initial

--- components/Greetings.js
+++ components/Greetings.js
@@ -11,7 +11,7 @@ const Greetings = () => {
                     <div className={ style.headerContent }>
                         <h1>{greetings.title}</h1>
                         <p>{greetings.description}</p>
-                        <a href="https://drive.google.com/file/d/19o8HxPWhzeOA43GxCL9ye_7j-FE8vrAj/view?usp=sharing" target="_blank">Resume</a>
+                        <a href="https://drive.google.com/file/d/19o8HxPWhzeOA43GxCL9ye_7j-FE8vrAj/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
                     </div>
                     <div className={style.heroImg}>
                         <img src='/images/heroImage.svg' alt="hero-image"></img>
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -2,7 +2,7 @@ import style from '../styles/Projects.module.scss';
 
 const ProjectCard = ({ project }) => {
     return (
-        <a href={project.fields.projectLink} target="_blank">
+        <a href={project.fields.projectLink} target="_blank" rel="noreferrer">
             <div className={style.projectCard}>
                 <img src={'https:' + project.fields.image.fields.file.url} alt="project Image"></img>
                 <div className={style.cardContent}>
