61191822b997c2d11b4525861a4b5c4be35e2a7e
lunghile-suc <lunghilesuccess1@gmail.com>
2021-12-11 09:50:20 +0200

    modified resume link

--- components/Greetings.js
+++ components/Greetings.js
@@ -11,7 +11,7 @@ const Greetings = () => {
                     <div className={ style.headerContent }>
                         <h1>{greetings.title}</h1>
                         <p>{greetings.description}</p>
-                        <a href="https://drive.google.com/file/d/1IG1rUizKQWqh8SwCW86qXdzYGWvps25x/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
+                        <a href="https://drive.google.com/file/d/1haOo5trbe4fc6owvQjmSwN-_QVKGjC1C/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
                     </div>
                     <div className={style.heroImg}>
                         <img src='/images/heroImage.svg' alt="hero-image"></img>
